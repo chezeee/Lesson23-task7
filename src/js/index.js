6513d0cb7d374b4ae5b9a1440ad273cd7647cd2d
Ilya Yalymov <chezeee@gmail.com>
2023-03-10 21:48:30 +0300

    Solution added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,15 @@
 const initGetWeather = () => {
-  // your code
+  const btn = document.querySelector("#my-button");
+
+  btn.addEventListener("click", () => {
+    btn.setAttribute("disabled", "disabled");
+    btn.textContent = "Loading...";
+
+    setTimeout(() => {
+      btn.removeAttribute("disabled");
+      btn.textContent = "Get weather";
+    }, 2000);
+  });
 };
 
 // Sample usage - do not modify
